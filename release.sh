65bcc4b03d0efb41028ecf55c179e244374e7e63
arungeorgesaji <arungeorgesaji@gmail.com>
2024-11-10 19:45:39 +0530

    Changed all references in release.sh

--- release.sh
+++ release.sh
@@ -1,22 +1,22 @@
 #!/bin/bash 
 
-echo "Shutting down the Arch VM..." 
+echo "Shutting down the NetBSD VM..." 
 
-echo y | ship --vm shutdown arch-vm-base 
+echo y | ship --vm shutdown netbsd-vm-base 
 
-echo "Compressing the Arch VM disk image..."
+echo "Compressing the NetBSD VM disk image..."
 
-ship --vm compress arch-vm-base 
+ship --vm compress netbsd-vm-base 
 
-echo "Copying the Arch VM disk image to generate the release package for 'arch-vm-base'..."
+echo "Copying the NetBSD VM disk image to generate the release package for 'netbsd-vm-base'..."
 
-DISK_IMAGE=$(sudo virsh domblklist arch-vm-base | grep .qcow2 | awk '{print $2}')
+DISK_IMAGE=$(sudo virsh domblklist netbsd-vm-base | grep .qcow2 | awk '{print $2}')
 
-cp "$DISK_IMAGE" output/archlinux.qcow2
+cp "$DISK_IMAGE" output/netbsd.qcow2
 
 echo "Splitting the copied disk image into two parts..."
 
-split -b $(( $(stat -c%s "output/archlinux.qcow2") / 2 )) -d -a 3 "output/archlinux.qcow2" "output/archlinux.qcow2."
+split -b $(( $(stat -c%s "output/netbsd.qcow2") / 2 )) -d -a 3 "output/netbsd.qcow2" "output/netbsd.qcow2."
 
-echo "The release package for 'arch-vm-base' has been generated and split successfully!"
+echo "The release package for 'netbsd-vm-base' has been generated and split successfully!"
 
